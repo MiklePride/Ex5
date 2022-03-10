d7795db268a40a2f1b484223b86ae118e8e4b532
Михаил Денисов <migelius.invesy@gmail.com>
2022-03-10 17:18:35 +0300

    Добавьте файлы проекта.

---
+++ Ex5/Program.cs
@@ -0,0 +1,17 @@
+﻿class program
+{
+    static void Main(string[] args)
+    {
+        string firstName = "Денисов ";
+        string lastName = "Михаил ";
+        Console.WriteLine(firstName + lastName);
+
+        string bufferVariable = firstName;
+        firstName = lastName;
+        lastName = bufferVariable;
+        Console.WriteLine(firstName + lastName);
+    }
+}
+
+
+   
